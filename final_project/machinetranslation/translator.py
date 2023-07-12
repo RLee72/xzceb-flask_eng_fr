9fa031af63086035c8987700bce1f482dc498081
Rian L <32441927+RLee72@users.noreply.github.com>
2023-07-12 11:16:23 -0400

    Update translator.py

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,12 +1,13 @@
 """importing deep_translator"""
 from deep_translator import MyMemoryTranslator
 
-def englishToFrench(english_text):
+def english_to_french(english_text):
     """code for translation here"""
     french_text = MyMemoryTranslator(source='english',target='french').translate(english_text)
     return french_text
 
-def frenchToEnglish(french_text):
+def french_to_english(french_text):
     """code for translation here"""
     english_text = MyMemoryTranslator(source='french',target='english').translate(french_text)
     return english_text
+
